d123d42251a41b8cc0a02ce3cf8e3e7ac31e41d8
Marc <marccastello1205@gmail.com>
2022-06-14 12:11:07 +0200

    checkpoint 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,8 @@
 
-read_data = {open("stops.csv", mode="rt", encoding="utf-8"), 
+read_data = {open("stops.csv", mode="rt", encoding="utf-8"),
             open("stops_data.csv", mode="rt", encoding="utf-8")}
 
-print(read_data)
+dic = {
+    
+}
 
---
+++ ejercicio3.py
@@ -0,0 +1,5 @@
+
+f = open("results.json", mode="rt", encoding="utf-8")
+
+dic = {f}
+get_name_description = 
